f8063136b296a1a08889e952534cd1f527a6594f
Souradeep Saha <sahasouradeep01@gmail.com>
2020-06-05 18:17:56 -0400

    updated the portfolio

---
+++ assets/scripts/exhibition.js
@@ -0,0 +1,14 @@
+$.getJSON("../../submission.json", function(json) {
+  console.log(json);
+  for (let i = 0; i < json.length; i++) {
+    var card = `
+    <div class="card">
+      <img class="card-img-top" src="${json[i].URL}" alt="Card image cap">
+      <div class="card-body">
+        <p class="card-text">${json[i]['First-Name']} ${json[i]['Last-Name']}</p>
+        <p class="card-text">${json[i]['Artist-Statement']}</p>
+    </div>`
+    
+    $(".card-columns").append(card);
+  }
+});
